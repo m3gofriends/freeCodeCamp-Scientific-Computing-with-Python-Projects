179379d985e418ae8f2c3a0c41a430e9d20a8734
m3gofriends <m3gofriends@gmail.com>
2021-08-10 17:08:01 +0800

    Latest release

--- Time Calculator/time_calculator.py
+++ Time Calculator/time_calculator.py
@@ -19,7 +19,7 @@ def add_time(start, duration, day = None):
 
     if(new_time_minute // 60 != 0):
         new_time_hour = int(start_time[0]) + int(duration_time[0]) + new_time_minute // 60
-        new_time_minute = new_time_minute % 60
+        new_time_minute %= 60
     else:
         new_time_hour = int(start_time[0]) + int(duration_time[0])
 
@@ -27,23 +27,23 @@ def add_time(start, duration, day = None):
         padding = "0"
 
     while(new_time_hour > 24):
-        duration_day = duration_day + 1
-        new_time_hour = new_time_hour - 24
+        duration_day += 1
+        new_time_hour -= 24
       
     if(new_time_hour > 12):
         if(am_pm == "AM"):
             am_pm = "PM"
         else:
             am_pm = "AM"
-            duration_day = duration_day + 1
-        new_time_hour = new_time_hour - 12
+            duration_day += 1
+        new_time_hour -= 12
 
     if(new_time_hour % 12 == 0):
         if(am_pm == "AM"):
             am_pm = "PM"
         else:
             am_pm = "AM"
-            duration_day = duration_day + 1
+            duration_day += 1
      
     if day != None:
         new_day = list(day_dict.keys())[list(day_dict.values()).index((day_dict.get(day.lower()) + duration_day) % 7)]
@@ -53,8 +53,8 @@ def add_time(start, duration, day = None):
 
     if(duration_day):
         if(duration_day == 1):
-            new_time = new_time + " (next day)"
+            new_time += " (next day)"
         else:
-            new_time = new_time + " (" + str(duration_day) + " days later)"
+            new_time += " (" + str(duration_day) + " days later)"
   
     return new_time
